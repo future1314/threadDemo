6a732bc30963c72f407447617193ac45514f2103
Snaiclimb <koushuangbwcx@163.com>
2018-03-26 10:33:08 +0800

    管道输入输出流

---
+++ src/pipedInputOutput/ThreadWrite.java
@@ -0,0 +1,21 @@
+package pipedInputOutput;
+
+import java.io.PipedOutputStream;
+
+public class ThreadWrite extends Thread {
+
+	private WriteData write;
+	private PipedOutputStream out;
+
+	public ThreadWrite(WriteData write, PipedOutputStream out) {
+		super();
+		this.write = write;
+		this.out = out;
+	}
+
+	@Override
+	public void run() {
+		write.writeMethod(out);
+	}
+
+}
